aa1f39ef20a80b121f265579fe302b1d8f9296de
hkz404 <hankzhu91@gmail.com>
2024-10-04 22:27:46 +0800

    init 3d scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import * as THREE from 'three';
+import { Perf } from 'r3f-perf';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Loader, GizmoHelper, GizmoViewport, OrbitControls, Environment } from '@react-three/drei';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <Suspense fallback={null}>
+        <Canvas shadows dpr={[1, 2]} camera={{ position: [-5, 2, 10], fov: 30 }}>
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+
+          <ambientLight intensity={0.5} />
+          <Environment files="/adams_place_bridge_1k.hdr" />
+
+          <OrbitControls makeDefault />
+          <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+            <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white" />
+          </GizmoHelper>
+
+          <Perf position="top-left" />
+        </Canvas>
+      </Suspense>
+      <Loader />
+    </main>
   )
 }
 
